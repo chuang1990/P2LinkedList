87c9dfcb4c57f75495b22b2a8aacef2a5c43a4d8
chuang1990 <c_huang4@post.massbay.edu>
2014-10-08 08:22:45 -0400

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+/**
+ * Write a description of class Tester here.
+ * 
+ * @Catherine Huang
+ * @Oct. 5, 2014
+ */
+public class Tester
+{
+    
+
+    /**
+     * Constructor for objects of class Tester
+     */
+    public Tester()
+    {
+        // initialise instance variables
+    }
+
+    /**
+     * Main method that test the Trivia Game.
+     */
+    public static void main(String args[]){
+        GameGUI trivia = new GameGUI();
+        
+         
+    }
+}
